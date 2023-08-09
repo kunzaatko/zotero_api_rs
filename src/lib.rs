6dcdf1e6a61c6116b6efb1bb5aa4d0f84b03aac3
Martin Kunz <martinkunz@email.cz>
2023-08-10 01:39:21 +0200

    add: Formation of the request methods
    Added `set_url` and `add_default_headers` methods that will help form
    the api requests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
+// TODO: Documentation <09-08-23>
+// #![deny(missing_docs)]
+// #![deny(clippy::missing_docs_in_private_items)]
+#![allow(dead_code)]
+
 mod error;
 mod zotero_api;
+
+pub use crate::zotero_api::*;
--- src/zotero_api.rs
+++ src/zotero_api.rs
@@ -1,10 +1,6 @@
 use derive_builder::Builder;
 use log::{info, warn};
-use reqwest::{header, Client, Method, Request, Response};
-
-// TODO: When implementing async, this should be split into structs that hold the data for sending
-// the requests and the structs that hold the necessary request data. Then the process of creating
-// requests can be separate <01-08-23>
+use reqwest::{Client, Method, Request, Response};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum InteractionMode {
@@ -15,8 +11,8 @@ pub enum InteractionMode {
 impl InteractionMode {
     pub fn prefix(&self) -> String {
         match self {
-            Self::User(id) => format!("users/{id}").to_owned(),
-            Self::Group(id) => format!("groups/{id}").to_owned(),
+            Self::User(id) => format!("users/{id}/").to_owned(),
+            Self::Group(id) => format!("groups/{id}/").to_owned(),
         }
     }
 }
@@ -36,19 +32,65 @@ pub struct ZoteroAPI {
 }
 
 impl ZoteroAPIBuilder {
-    const BASE_URL: &str = "https://api.zotero.org";
+    const HOST: &str = "https://api.zotero.org";
 
     fn default_endpoint(&self) -> url::Url {
-        Self::BASE_URL.parse().expect("BASE_URL is valid")
+        Self::HOST.parse().expect("BASE_URL is valid")
     }
 }
 
 impl ZoteroAPI {
-    fn headers(&self) -> header::HeaderMap {
-        todo!()
+    fn insert_headers(&self, req: &mut Request) -> Result<(), Box<dyn std::error::Error>> {
+        let map = req.headers_mut();
+        map.reserve(2);
+        map.insert(
+            "Zotero-API-Version",
+            "3".parse().expect("3 is a valid HeaderValue"),
+        );
+        map.insert(
+            "Authorization",
+            format!("Bearer {}", &self.api_key)
+                .parse()
+                .expect("API key is valid"),
+        );
+        Ok(())
     }
 
-    async fn execute_request(&self, req: Request) -> Result<Response, reqwest::Error> {
+    fn set_url(
+        &self,
+        req: &mut Request,
+        path: &str,
+        args: Option<&[(&str, &str)]>,
+        query: Option<&str>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let url = req.url_mut();
+
+        #[cfg(test)]
+        assert_eq!(
+            url.as_str(),
+            self.endpoint.as_str(),
+            "Initial URL {} must be `self.endpoint()`",
+            url.as_str()
+        );
+
+        let usergroup_prefix = self.mode.as_ref().expect("Mode is defined").prefix();
+        url.set_path(&usergroup_prefix);
+        *url = url.join(path)?;
+        if let Some(args) = args {
+            *url = reqwest::Url::parse_with_params(url.as_str(), args)?;
+        };
+        if let Some(query) = query {
+            url.set_query(Some(query));
+        };
+
+        Ok(())
+    }
+
+    // TODO: API requests macro for common methods <09-08-23>
+    // TODO: Collections getter with the possibility of nested <09-08-23>
+    // TODO: Model feature with methods returning to model structs <09-08-23>
+
+    async fn execute(&self, req: Request) -> Result<Response, reqwest::Error> {
         if let Some(client) = &self.client {
             client.execute(req).await
         } else {
@@ -60,7 +102,7 @@ impl ZoteroAPI {
     }
 
     pub async fn fetch_userid(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let response = self.execute_request(self.get_key_info()).await?;
+        let response = self.execute(self.get_key_info()).await?;
         let user_id = &response.json::<serde_json::Value>().await?["userID"];
         self.mode = Some(InteractionMode::User(user_id.to_string()));
         Ok(())
@@ -114,6 +156,14 @@ mod tests {
             .unwrap()
     }
 
+    fn test_api_with_mode() -> ZoteroAPI {
+        ZoteroAPIBuilder::default()
+            .api_key(API_KEY)
+            .mode(InteractionMode::User(USER_ID.to_owned()))
+            .build()
+            .unwrap()
+    }
+
     #[test]
     fn get_key_info() {
         let api = test_api();
@@ -139,5 +189,37 @@ mod tests {
 
         assert!(api.mode.is_some());
         assert_eq!(api.mode, Some(InteractionMode::User(USER_ID.to_owned())));
+    }
+
+    #[test]
+    #[should_panic]
+    #[allow(unused_must_use)]
+    fn set_nonblank_url() {
+        let api = test_api();
+        api.set_url(&mut api.get_key_info(), "items", None, None);
+    }
+
+    #[test]
+    #[should_panic]
+    #[allow(unused_must_use)]
+    fn set_nomode_url() {
+        let api = test_api();
+        let mut req = Request::new(Method::GET, api.endpoint.clone());
+
+        api.set_url(&mut req, "items", None, None);
+    }
+
+    #[test]
+    fn set_url() {
+        let api = test_api_with_mode();
+        let mut req = Request::new(Method::GET, api.endpoint.clone());
+        let res = api.set_url(&mut req, "items", None, None);
+
+        assert!(res.is_ok());
+
+        assert_eq!(
+            req.url().as_str(),
+            format!("https://api.zotero.org/users/{}/items", USER_ID)
+        );
     }
 }
